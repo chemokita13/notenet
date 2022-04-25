a6bc5dd3be611cf7329e530b4de53cb8690da099
unknown <chemokita13@gmail.com>
2022-04-25 19:47:28 +0200

    1.0.4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 const app = require('./server')
 require('./db');
-console.log(process.env.TESTING)
 app.listen(app.get('port'), ()=>{
     console.log("server on port " + app.get('port'))
 })
